78c0bcdb23aa277c5dc7373bd0fda04cff9b0e70
Phillip <philmography2010@gmail.com>
2024-07-14 11:00:06 -0500

    update

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,1 +1,21 @@
-const test = require('../checkFive.js');
+const checfkFive = require('../checkFive.js');
+
+describe("checkFive", function(){
+
+    test("returns 'num is less than 5' when num < 5.", function(){
+        let input = 3
+        let result = checfkFive(input);
+        expect(result).toBe(`${input} is less than 5.`);
+    });
+    test("returns 'num is greater than 5' when num > 5.", function(){
+        let input = 6
+        let result = checfkFive(input);
+        expect(result).toBe(`${input} is greater than 5.`);
+    });
+    test("returns 'num is equeal to 5' when num = 5.", function(){
+        let input = 5
+        let result = checfkFive(input);
+        expect(result).toBe(`${input} is equal to 5.`);
+    });
+
+});
