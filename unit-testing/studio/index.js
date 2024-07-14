7a3f57e54c992ee1e3352c8bf0f30706a8c5d57b
Phil <philmography2010@gmail.com>
2024-07-14 12:35:34 -0500

    studio work

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,7 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executive
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,7 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test('organization key returns value of "nonprofit', function(){
+    expect(launchcode())
+  });
 });
