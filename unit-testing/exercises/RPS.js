9645a43f82e0efcf46d1086888ff8637bdb9988c
Phil <philmography2010@gmail.com>
2024-07-14 12:12:38 -0500

    unit exercises

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -1,22 +1,28 @@
 function whoWon(player1,player2){
-
-   if (player1 === player2){
+  const choices = ['rock', 'paper', 'scissors'];
+  const player1Lower = player1.toLowerCase();
+  const player2Lower = player2.toLowerCase();
+  if(!choices.includes(player1Lower)||!choices.includes(player2Lower)){
+    return "INVALID MOVE!"
+  }
+   if (player1Lower === player2Lower){
      return 'TIE!';
    }
    
-   if (player1 === 'rock' && player2 === 'paper'){
+   if (player1Lower === 'rock' && player2Lower === 'paper'){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'paper' && player2 === 'scissors'){
+   if (player1Lower === 'paper' && player2Lower === 'scissors'){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'scissors' && player2 === 'rock '){
+   if (player1Lower === 'scissors' && player2Lower === 'rock '){
      return 'Player 2 wins!';
    }
  
    return 'Player 1 wins!';
  };
+ console.log(whoWon('rock', 'Paper'));
 /* console.log(whoWon('rock','rock')); */
  module.exports = whoWon;
--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -6,9 +6,16 @@ describe('RPS', function(){
         let result = whoWon('rock','rock');
         expect(result).toBe('TIE!');
     });
-    test('returns "Player 2 wins!" when player to use', function(){
+    test('returns "Player 2 wins!"', function(){
         let result = whoWon('rock','paper') || whoWon('paper','scissors') || whoWon('scissors','rock');
         expect(result).toBe('Player 2 wins!');
     });
+    test('returns "Player 1 wins!" ', function(){
+        let result = whoWon('Paper','rock') || whoWon('scissors','paper') || whoWon('rock','scissors');
+        expect(result).toBe('Player 1 wins!');
+    });
+    test('invalid move" ', function(){
+        let result = whoWon('hand','fist')
+        expect(result).toBe('INVALID MOVE!');
+    });
 });
-
