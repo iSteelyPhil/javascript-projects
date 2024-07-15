7711e88fbfac19dc03fc3d807e3b55d83bc96902
Phil <philmography2010@gmail.com>
2024-07-15 17:52:34 -0500

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,19 @@
-
 let launchcode = {
   organization: "nonprofit",
-  executive
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(input){
+    let result = "";
+    if(input %2===0){
+   result += "Launch";
+    }if(input  % 3===0){
+      result += "Code";
+    }if(input  % 5===0){
+      result += "Rocks";
+    } 
+    return result +"!"
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -5,6 +5,33 @@ describe("Testing launchcode", function(){
 
   // Write your unit tests here!
   test('organization key returns value of "nonprofit', function(){
-    expect(launchcode())
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
+  test('percentageemployees key returns value of "nonprofit', function(){
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+  test('programsOffered key returns value of array', function(){
+    expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+  });
+  test('programsOffered key returns value of array', function(){
+    expect(launchcode.programsOffered[0]).toEqual("Web Development");
+  });
+  test('programsOffered key returns length of array', function(){
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
+  describe('Should have a method called launchOutput, which', function(){
+    
+    test('should return "Launch!" for numbers evenly divisible by 2', function(){
+      expect(launchcode.launchOutput(2)).toEqual('Launch!');
+    });
+    test('should return "Launch!" for numbers evenly divisible by 3', function(){
+      expect(launchcode.launchOutput(3)).toEqual('Code!');
+    });
+    test('should return "Launch!" for numbers evenly divisible by 5', function(){
+      expect(launchcode.launchOutput(5)).toEqual('Rocks!');
+    });
+    test('should return "LaunchCode!" for numbers evenly divisible by 2 and 3', function(){
+      expect(launchcode.launchOutput(6)).toEqual('LaunchCode!');
+    });
   });
 });
