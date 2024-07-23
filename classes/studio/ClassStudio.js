f04ff51a6512022ecbc9a4b8566a9b297114c8ef
Phil <philmography2010@gmail.com>
2024-07-22 21:30:15 -0500

    test

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -6,23 +6,27 @@ class CrewCandidate {
     this.scores = scores;
     
     }
-    avgScore = function(scores){
-        this.scores = 0
-        for(let i = 0; i<scores.length;i++){
-            this.scores+= scores[i]
+    addScore = function(newScore){
+        return this.scores.push(newScore)
+    };
+
+    avgScore = function(){
+       let avgSum = 0
+        for(let i = 0; i<this.scores.length;i++){
+            avgSum += this.scores[i]
         }
-        return this.score = Math.round(this.scores/scores.length)
+        return avgSum= Math.round(avgSum/this.scores.length)
     }
     
-
     status(){
-        if(avgScore >= 90){
+        let avg = this.avgScore();
+        if(avg >= 90){
             return "Accepted"
-        }else if( avgScore >80){
+        }else if( avg >80){
             return 'Reserve'
-        }else if( avgScore >70){
+        }else if( avg >70){
             return 'Probationary'
-        }else{
+        }else if(avg <70 ){
             return 'rejection'
         }
     }
@@ -30,14 +34,13 @@ class CrewCandidate {
 let bear = new CrewCandidate('Bubba Bear', 135, [88,85,90]);
 let merry = new CrewCandidate('Merry Maltese', 1.5, [93,88,97]);
 let gator = new CrewCandidate('Glad Gator', 225, [75,78,62]);
-/* console.log(bear); */
 
 
-//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+/* //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 CrewCandidate.prototype.addScore = function(newScore){
     return this.scores.push(newScore)
 };
-
+ */
 
 
 /* CrewCandidate.prototype.avgScore = function(scores){
@@ -48,14 +51,41 @@ CrewCandidate.prototype.addScore = function(newScore){
     return this.score = Math.round(this.scores/scores.length)
 } */
 
-bear.avgScore(bear.scores)
-console.log(bear)
-merry.avgScore(merry.scores)
-console.log(merry)
-gator.avgScore(bear.scores)
+/* console.log(bear.avgScore()) */
+console.log(merry.status())
 
-CrewCandidate.prototype.status() = function(){
 
-}
-bear.status()
+console.log(`${bear.name} earned an average test score of ${bear.avgScore()}% and has a status of ${bear.status()}.`);
+console.log(`${merry.name} earned an average test score of ${merry.avgScore()}% and has a status of ${merry.status()}.`);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+
+
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
+/* gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
+gator.addScore(100);
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`); */
+let gatorLoop = function(){
+    let tests = gator.avgScore();
+   while(tests<90){
+        if(tests<= 90){
+            gator.addScore(100)
+            gatorLoop()
+        }else{
+            return gator.scores.length
+        }
+        
+    }
+    
+}
+console.log(gatorLoop());
+/* console.log(`It took ${gator.name} ${gatorLoop(gator.avgScore())} tests to finally reach ${gator.status()}!`); */
+console.log(`${gator.name} earned an average test score of ${gator.avgScore()}% and has a status of ${gator.status()}.`);
