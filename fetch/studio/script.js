3c971f7761e0e02d965b57b9e6563a1014af92fb
Phillip <philmography2010@gmail.com>
2024-08-12 19:44:23 -0500

    studio fetch complete

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,28 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function () {
+  this.fetch(
+    "https://handlers.education.launchcode.org/static/astronauts.json"
+  ).then(function (response) {
+    response.json().then(function (json) {
+      json.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+      const astronautCount = document.getElementById("astronautCount");
+      const astronaut = document.getElementById("container");
+      for (i = 0; i < json.length; i++) {
+        astronautCount.innerHTML = ` Astronaut Count: ${json.length}`;
+        astronaut.innerHTML += `<div class="astronaut">
+    <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${json[i].hoursInSpace}</li>
+        <li ${json[i].active ? 'class="isActive"' : ""}>Active: ${
+          json[i].active
+        }</li>
+        <li>Skills: ${json[i].skills.join(", ")}</li>
+        </ul>
+    </div>
+    <img class="avatar" src=${json[i].picture}>
+</div>`;
+      }
+    });
+  });
+});
